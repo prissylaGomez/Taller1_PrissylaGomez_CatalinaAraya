81a977b09e82cdcfe4fb6dc8e1aadeb3bb997ccb
CatLimon <85632239+CatLimon@users.noreply.github.com>
2025-09-12 20:47:08 -0300

    documentacion estudiante.h

--- scr/estudiante.h
+++ scr/estudiante.h
@@ -1,3 +1,10 @@
+/*
+estudiante.h
+el header tiene:
+el constructor de la clase Estudiante
+la estructura del NodoEstudiante
+y los metodos para manejar estudiantes en memoria
+*/
 #ifndef estudiante_h
 #define estudiante_h
 
@@ -5,7 +12,7 @@
 #include <iostream>
 #include "Curso.h"
 using namespace std;
-
+//constructor 
 class Estudiante{
     private:
         string id;
@@ -15,29 +22,30 @@ class Estudiante{
         string ingreso;
 
     public:
-        Estudiante(){}
+        Estudiante(){}//constructor vacio
         Estudiante(string id, string nombre, string apellido, string carrera, string ingreso);
         ~Estudiante();
-        
+        //getters
         string getId();
         string getNombre();
         string getApellido();
         string getCarrera();
         string getIngreso();
 };
+//nodo de la lista enlazada de estudiantes
 struct NodoEstudiante {
-    Estudiante data;
+    Estudiante data; //aqui se esta almacenando el estudiante 
     NodoEstudiante* next;
 };
-
+//lista enlazada admin estudiantes
 class ListEstudiantes {
     private: 
     NodoEstudiante* head;
     
     public:
-    ListEstudiantes();
-    ~ListEstudiantes();
-    
+    ListEstudiantes(); //constructor 
+    ~ListEstudiantes();//destructor
+    //opciones de la lista
     void agregarEstudiante(string id, string nombre, string apellido, string carrera, string ingreso);
     void buscarEstudiante(string id);
     void eliminarEstudiante(string id);
