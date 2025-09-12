8710f477cc75a36d11d33985a7d4bd839778600d
CatLimon <85632239+CatLimon@users.noreply.github.com>
2025-09-12 20:54:53 -0300

    documentacio_Curso.h

--- scr/Curso.h
+++ scr/Curso.h
@@ -1,10 +1,15 @@
+/*
+curso.h
+este header tiene:
+constructor, el nodo con la lista enlazada y el manejo de la lista de cursos.
+*/
 #ifndef curso_h
 #define curso_h
 
 #include <iostream>
 #include <string>
 using namespace std;
-
+//constructor
 class Curso {
 private:
     string codigo;
@@ -14,31 +19,33 @@ private:
     string profesor;
 
 public:
-    Curso(){}
+    Curso(){}//constructor vacio como en los otros header
     Curso(string c, string n, int max, string car, string profe);
     ~Curso();
-
+//los getters
     string getCodigo();
     string getNombre();
     int getEstudiantesMax();
     string getCarrera();
     string getProfesor();
     
-};
+};//nodo lista enlazada de cursos
 struct NodoCursos {
-    Curso data;
+    Curso data;//aqui se guarda el cursito
     NodoCursos* next;
 };
+//lista enlazada para admin de curso
 class ListCursos{
     private:
         NodoCursos* inicio;
     public:
         ListCursos();
         ~ListCursos();
-        
+        //funciones principales
         void agregarCurso(string codigo, string nombre, int max, string carrera, string profesor);
         void eliminarCurso(string codigo);
         void mostrarCurso(string codigo);
         Curso* buscarPorCodigo(string codigo);
 };
 #endif
+//ya eso
