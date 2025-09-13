697956a61c19e501310cd643b6f91ffba6a858bf
CatLimon <85632239+CatLimon@users.noreply.github.com>
2025-09-12 21:05:19 -0300

    mas_docu_inscripcion.h

--- scr/inscripcion.h
+++ scr/inscripcion.h
@@ -1,3 +1,8 @@
+/*
+Inscripcion.h
+lista enlazada de notas de por estudiante en un curso, la relacion entre estudiante y curso atravez de NodoInscripcion
+y una lista enlazada de inscripciones.
+*/
 #ifndef INSCRIPCION_H
 #define INSCRIPCION_H
 
@@ -13,24 +18,25 @@ struct NodoNota{
     NodoNota* next;
 };
 
-// Nodo de inscripcion: relaciona idAlumno  con codigoCurso y la lista de notas
+//  relaciona idAlumno  con codigoCurso y la lista de notas
 struct NodoInscripcion{
     string idAlumno;
     string codigoCurso;
     NodoNota* notasHead; 
     NodoInscripcion* next;
 };
-
+//la lista enlazada de inscripciones
 class ListInscripciones{
 private:
     NodoInscripcion* head;
 public:
     ListInscripciones();
     ~ListInscripciones();
-
+//funciones de la clase
     void inscribir(string idAlumno, string codigoCurso);
     void eliminarInscripcion(string idAlumno, string codigoCurso);
    
 };
 
 #endif
+
